a3cf5a2222afcdde3aba4d1e7ed3d282b990cbf0
alswjd1125 <alslal1125@naver.com>
2020-05-22 10:19:36 +0900

    조별과제
    20200522 - 박민정 조 조별과제

---
+++ home/js/script.js
@@ -0,0 +1,19 @@
+
+document.getElementById("btn-nav").onclick = function() {
+    myFunction()};
+
+function myFunction() {
+    document.getElementById("menu_box").classList.toggle("show");
+}
+
+// var nav=document.getElementById('nav');
+
+// document.getElementById('btn-nav').onclick = function(){
+//     nav.style.display="block";
+//     nav.style.position="absolute";
+//     nav.style.top="64px";
+//     nav.style.left="0px";
+//     nav.style.background="#343a40";
+//     nav.style.width="100%";
+    
+// }

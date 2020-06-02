c75e7b4e2f0ba84486bd01a8ed1e99954bfa352f
alswjd1125 <alslal1125@naver.com>
2020-06-02 12:23:11 +0900

    웽ㅇ

---
+++ webdesign/js/js.js
@@ -0,0 +1,8 @@
+setInterver(function(){
+    $(".slide_box").delay("2500");
+    $(".slide_box").animate({marginTop:"-300"},"500");
+    $(".slide_box").delay("2500");
+    $(".slide_box").animate({marginTop:"-600"},"500");
+    $(".slide_box").delay("2500");
+    $(".slide_box").animate({marginTop:"0"},"500");
+});

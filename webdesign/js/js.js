9621f41031313ec036518dd2895ee1e0a25aacb0
bab <eyeyaaa84@gmail.com>
2020-07-13 12:02:38 +0900

    dsf

--- webdesign/js/js.js
+++
@@ -1,8 +0,0 @@
-setInterver(function(){
-    $(".slide_box").delay("2500");
-    $(".slide_box").animate({marginTop:"-300"},"500");
-    $(".slide_box").delay("2500");
-    $(".slide_box").animate({marginTop:"-600"},"500");
-    $(".slide_box").delay("2500");
-    $(".slide_box").animate({marginTop:"0"},"500");
-});
